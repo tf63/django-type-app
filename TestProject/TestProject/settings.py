e3b0ce9fd4857c1571e2435a52dce09c0e981d98
tf63 <tfukuda9031@gmail.com>
2023-04-29 07:05:41 +0000

    fix ci (postgres)

--- TestProject/TestProject/settings.py
+++ TestProject/TestProject/settings.py
@@ -87,16 +87,19 @@ DATABASES = {
         "HOST": "postgres",
         "PORT": 5432,
     },
-    "test": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_NAME"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": "postgres",
-        "PORT": "5432",
-    },
 }
 
+# if os.environ.get('GITHUB_WORKFLOW'):
+#     DATABASES = {
+#         'default': {
+#            'ENGINE': 'django.db.backends.postgresql',
+#            'NAME': 'github_actions',
+#            'USER': 'postgres',
+#            'PASSWORD': 'postgres',
+#            'HOST': '127.0.0.1',
+#            'PORT': '5432',
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
