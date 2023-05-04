1d5a9f14dad5ae69bfb483199d8eead0dcb49b31
tf63 <tfukuda9031@gmail.com>
2023-05-04 16:45:16 +0000

    add multi-line system

--- TypeProject/TypeApp/views.py
+++ TypeProject/TypeApp/views.py
@@ -6,11 +6,48 @@ from string import ascii_lowercase
 
 
 def start_game(request):
+    # wordsとtab_countsの長さが同じとなるようにする
+
     # word = "".join([choice(ascii_lowercase) for i in range(50)])
-    word = "aiueo kakikukeko sasisuseso tatituteto naninuneno hahihuheho mamimumemo yayuyo rarirurero wawon"
-    words = ["aiueo", "\tkakikukeko"]
+    # word = "aiueo kakikukeko sasisuseso tatituteto naninuneno hahihuheho mamimumemo yayuyo rarirurero wawon"
+    word = """class BankAccount:
+    def __init__(self, name, balance=0):
+        self.name = name
+        self.balance = balance
+    
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"{self.name}さんの口座に{amount}円を預けました。残高は{self.balance}円です。")
+    
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            print(f"{self.name}さんの口座から{amount}円を引き出しました。残高は{self.balance}円です。")
+        else:
+            print(f"残高不足です。{self.name}さんの口座には{self.balance}円しかありません。")
+    
+    def check_balance(self):
+        print(f"{self.name}さんの残高は{self.balance}円です。")"""
+
+    words = []
+    tab_counts = []
+    for i, w in enumerate(word.split("\n")):
+        tab_count = 0
+        w_striped = w.lstrip("\t")
+        tab_count += len(w) - len(w_striped)
+        w_striped = w.lstrip(" ")
+        tab_count += int((len(w) - len(w_striped)) / 4)
+
+        words.append(w_striped)
+        tab_counts.append(tab_count)
+
     time_limit = 60
-    data = {"word": word, "words": words, "time_limit": time_limit}
+    data = {
+        "word": word,
+        "words": words,
+        "tab_counts": tab_counts,
+        "time_limit": time_limit,
+    }
     return JsonResponse(data)
 
 
--- TypeProject/static/js/type.js
+++ TypeProject/static/js/type.js
@@ -1,104 +1,122 @@
-// function check(data) {
-//     $('#current_word').text(data.word);
-//     var timeLeft = data.time_limit;
-//     $('#time_left').text(timeLeft);
-//     var intervalId = setInterval(function () {
-//         timeLeft--;
-//         $('#time_left').text(timeLeft);
-//         if (timeLeft === 0) {
-//             clearInterval(intervalId);
-//             $('#submit_button').prop('disabled', true);
-//         }
-//     }, 1000);
-//     $('#submit_button').click(function () {
-//         var inputWord = $('#input_word').val();
-//         if (inputWord === data.word) {
-//             $('#result').text('Correct!');
-//         } else {
-//             $('#result').text('Incorrect...');
-//         }
-//     });
-// }
-
-function setCaret(sentence, index) {
+function setCaret(sentence, index, index_line) {
     let pressed = sentence.substring(0, index);
     pressed = pressed.replace(/ /g, "&nbsp;")
-    $("#pressed").html(pressed);
-    $("#target").html(
+
+    $(`#target_pressed${index_line}`).html(pressed);
+    $(`#target_text${index_line}`).html(
         "<span class='caret'></span>" + sentence.substring(index)
     );
 }
 
-// HTMLがロードされたら
-$(document).ready(function () {
-
-    let sentence = "none";
-    let timeLeft = 0;
-    let words = [];
-    $.ajax({
-        url: '/start_game/',
-        success: function (data) {
-            let sentenc = data.word;
-            words = data.words
-
-            timeLeft = data.time_limit;
-        }
-    });
-
-    console.log(words[0]);
-    sentence = words[0];
-    sentence = "\taaaa";
-    if (sentence.startsWith('\t')) {
-        console.log('タブから始まる');
-    }
-    let index = 0;
-    let incorrect = 0;
-    $("#target").text(sentence);
-    setCaret(sentence, index);
-
+function setTimer(timeLeft) {
+    // タイマー表示
     $('#time_left').text(timeLeft);
     let intervalId = setInterval(function () {
         timeLeft--;
         $('#time_left').text(timeLeft);
         if (timeLeft === 0) {
             clearInterval(intervalId);
-            $('#submit_button').prop('disabled', true);
+            // $('#submit_button').prop('disabled', true);
         }
     }, 1000);
+}
 
+// HTMLがロードされたら
+$(document).ready(async function () {
 
+    // api
+    const response = await $.ajax({
+        url: '/start_game/',
+    });
+
+    // inputs
+    let word = response.word;
+    let words = response.words;
+    let num_lines = words.length;
+    let tab_counts = response.tab_counts;
+    let timeLeft = response.time_limit;
+    setTimer(timeLeft);
+
+    // settings
+    let tab_space_width = 4;
+
+    // test
+    console.log(`words length: ${words.length}, tab_counts: length: ${tab_counts.length}`)
+
+    // variables
+    let index = 0;
+    let index_line = 0;
+    let incorrect = 0;
+    let sentence = words[index_line];
+
+    // set indent
+    for (let i = 0; i < num_lines; i++) {
+        let space = '&nbsp;'.repeat(tab_space_width).repeat(tab_counts[i]);
+        let space_prefix = `<span>${space}</span>`
+        let pressed_prefix = `<span id='target_pressed${i}' class='pressed'></span>`
+        let new_item = $("<li>").attr("class", "target_item");
+        new_item.html(space_prefix + pressed_prefix + `<span id='target_text${i}'>` + words[i] + "</span>");
+        $('#target_list').append(new_item);
+    }
+
+    setCaret(sentence, index, index_line);
+
+    // catch keydown
     $(document).on("keydown", function (event) {
 
         // タブキーが押された場合、デフォルトの動作をキャンセルする
         if (event.keyCode == "9") {
             event.preventDefault();
         }
-    })
 
-    $(document).on("keypress", function (event) {
-        let key = event.which;
+        // タブキーが押された場合、デフォルトの動作をキャンセルする
+        if (event.keyCode == "32") {
+            event.preventDefault();
+        }
+
+        let key = event.key;
         console.log(`キー ${key} が押されました`);
-
+        if (event.keyCode === 32) { // スペースキーが押された場合
+            console.log('Space key pressed!');
+        }
         // shiftキーが押されている場合、文字列を大文字に変換する
         if (event.shiftKey) {
             key = key.toUpperCase();
         }
 
-        if (key === sentence.charAt(index)) {
-            index++;
-            setCaret(sentence, index);
+        if (index != sentence.length) {
 
-            $('#result_text').text('Correct!');
-            $('#correct').text(index);
-            if (index === sentence.length) {
+            if (key == sentence.charAt(index)) {
+                index++;
+                setCaret(sentence, index, index_line);
 
-                $('#result').text('Congratulations! You have completed the game.');
+                $('#result').text('Correct!');
+                $('#correct').text(index);
+            } else {
+                incorrect++;
+                $('#result').text('Incorrect...');
+                $('#incorrect').text(incorrect);
             }
+
         } else {
-            incorrect++;
-            // $("#input").val("");
-            $('#result_text').text('Incorrect...');
-            $('#incorrect').text(incorrect);
+            if (key == "Enter") {
+                $(`#target_text${index_line}`).html("");
+                index_line++;
+                $('#result').text('complete line');
+                sentence = words[index_line];
+                index = 0;
+                setCaret(sentence, index, index_line);
+            } else {
+                incorrect++;
+                $('#result').text('Incorrect...');
+                $('#incorrect').text(incorrect);
+            }
         }
-    });
+
+
+        if (index_line == num_lines) {
+            $('#result').text('complete !!!')
+            // エラー処理
+        }
+    })
 });
