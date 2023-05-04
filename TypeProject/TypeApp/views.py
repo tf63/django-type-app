57f6bb1037b245583686946655f081643b03167c
tf63 <tfukuda9031@gmail.com>
2023-05-04 14:35:04 +0000

    add js

--- TypeProject/TypeApp/views.py
+++ TypeProject/TypeApp/views.py
@@ -6,9 +6,11 @@ from string import ascii_lowercase
 
 
 def start_game(request):
-    word = "".join([choice(ascii_lowercase) for i in range(5)])
+    # word = "".join([choice(ascii_lowercase) for i in range(50)])
+    word = "aiueo kakikukeko sasisuseso tatituteto naninuneno hahihuheho mamimumemo yayuyo rarirurero wawon"
+    words = ["aiueo", "\tkakikukeko"]
     time_limit = 60
-    data = {"word": word, "time_limit": time_limit}
+    data = {"word": word, "words": words, "time_limit": time_limit}
     return JsonResponse(data)
 
 
