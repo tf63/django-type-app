a1771602d19527b0375f38510cfcc7f1c848178c
tf63 <tfukuda9031@gmail.com>
2023-06-10 23:22:09 +0900

    feat: add problem name to problem api

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,42 +1,16 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-
+from .types import LANGUAGE_CHOICES, LENGTH_CHOICES
 
 class Problem(models.Model):
-    LANGUAGE_CHOICES = (
-        ('python', 'Python'),
-        ('java', 'Java'),
-        ('javascript', 'JavaScript'),
-        ('c', 'C'),
-        ('cpp', 'C++'),
-        ('csharp', 'C#'),
-        ('php', 'PHP'),
-        ('ruby', 'Ruby'),
-        ('go', 'Go'),
-        ('swift', 'Swift'),
-        ('kotlin', 'Kotlin'),
-        ('rust', 'Rust'),
-        ('typescript', 'TypeScript'),
-        ('html', 'HTML'),
-        ('css', 'CSS'),
-        ('sql', 'SQL'),
-        ('bash', 'Bash'),
-        ('powershell', 'PowerShell'),
-    )
-
-    LENGTH_CHOICES = (
-        ('short', 'short'),
-        ('medium', 'medium'),
-        ('long', 'long'),
-    )
-    
+    problem_name = models.CharField(max_length=100, default="")
     language = models.CharField(max_length=100, choices=LANGUAGE_CHOICES)
     length = models.CharField(max_length=10, choices=LENGTH_CHOICES)
     words = ArrayField(models.CharField(max_length=200), default=list)
     tab_counts = ArrayField(models.IntegerField(), default=list)
 
     def __str__(self):
-        return f"Problem {self.pk}, Lang: {self.language}, length: {self.length}"
+        return f"{self.problem_name}, Lang: {self.language}, length: {self.length}"
 
 class Record(models.Model):
     correct = models.IntegerField()
---
+++ backend/api/types.py
@@ -0,0 +1,26 @@
+LANGUAGE_CHOICES = (
+    ('python', 'Python'),
+    ('java', 'Java'),
+    ('javascript', 'JavaScript'),
+    ('c', 'C'),
+    ('cpp', 'C++'),
+    ('csharp', 'C#'),
+    ('php', 'PHP'),
+    ('ruby', 'Ruby'),
+    ('go', 'Go'),
+    ('swift', 'Swift'),
+    ('kotlin', 'Kotlin'),
+    ('rust', 'Rust'),
+    ('typescript', 'TypeScript'),
+    ('html', 'HTML'),
+    ('css', 'CSS'),
+    ('sql', 'SQL'),
+    ('bash', 'Bash'),
+    ('powershell', 'PowerShell'),
+)
+
+LENGTH_CHOICES = (
+    ('short', 'short'),
+    ('medium', 'medium'),
+    ('long', 'long'),
+)
